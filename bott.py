65222ceac747c62d479280acda837e88579c2404
Sharath S <sharathsv06@gmail.com>
2026-08-23 16:31:02 +0000

    bott.py

--- bott.py
+++ bott.py
@@ -136,7 +136,7 @@ YTDL_OPTS = {
     "cookiefile": "/home/container/cookies.txt",
     "extractor_args": {
         "youtube": {
-            "player_client": ["android", "web"]
+            "player_client": ["web_embedded"]
         }
     }
 }
